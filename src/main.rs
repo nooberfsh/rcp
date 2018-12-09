687c43b2dda188f91d6b996dde5572aec9c093c4
nooberfsh <nooberfsh@gmail.com>
2018-12-09 22:17:53 +0800

    Add recv

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,8 @@ fn main() {
         private_key: "/home/nooberfsh/.ssh/id_rsa_qiniu".parse().unwrap(),
     };
     let conn = Connection::connect(&config).unwrap();
-    conn.send("123.txt", "xs5:~/huajiongjiong/rcp/").unwrap();
+    // conn.send("123.txt", "xs5:~/huajiongjiong/rcp/").unwrap();
+    conn.recv(".", "xs5:~/huajiongjiong/server.sh").unwrap();
 }
 
 #[derive(Debug)]
@@ -25,6 +26,7 @@ enum Error {
     Io(io::Error),
     Ssh(ssh2::Error),
     Cmd(String),
+    InvalidRemoteAddr,
 }
 
 impl From<io::Error> for Error {
@@ -63,39 +65,49 @@ impl Connection {
         Ok(Connection::new(sess, tcp))
     }
 
-    fn send<P: AsRef<Path>>(&self, local: P, remote: &str) -> Result<(), Error> {
-        //create a tmp directory
-        let uuid = Uuid::new_v4();
-        let mkdir = format!("mkdir {}", uuid);
-        let code = self.exec(&mkdir)?;
-        if code != 0 {
-            let err = format!("create dir {} failed", uuid);
-            return Err(Error::Cmd(err))
-        }
-        
-        struct Clean<'a>(&'a Connection, &'a Uuid);
-        impl<'a> Drop for Clean<'a> {
-            fn drop(&mut self) {
-                let cmd = format!("rm -fr {}", self.1);
-                let code = self.0.exec(&cmd).expect("clean failed");
-                if code != 0 {
-                    println!("clean failed");
-                }
+    fn recv<P: AsRef<Path>>(&self, local: P, remote: &str) -> Result<(), Error> {
+        let dir = self.create_dir()?;
+        let _clean = Clean(self, dir.clone());
+
+        let cmd = format!("qscp {} {}", remote, dir);
+        self.exec(&cmd)?;
+
+        let name = match extract_file_name(remote) {
+            Some(f) => f,
+            None => return Err(Error::InvalidRemoteAddr),
+        };
+
+        let p = local.as_ref();
+        let mut local_file = if p.is_file() {
+            File::open(p)?
+        } else {
+            if !p.is_dir() {
+                fs::create_dir_all(p)?;
             }
-        }
-        let _clean = Clean(self, &uuid);
-    
+            let p = local.as_ref().to_path_buf();
+            File::create(p.join(&name))?
+        };
+
+        let remote: PathBuf = dir.into();
+        let remote = remote.join(&name);
+
+        let (mut remote_file, _) = self.sess.scp_recv(&remote)?;
+        io::copy(&mut remote_file, &mut local_file)?;
+        Ok(())
+    }
+
+    fn send<P: AsRef<Path>>(&self, local: P, remote: &str) -> Result<(), Error> {
+        let dir = self.create_dir()?;
+        let _clean = Clean(self, dir.clone());
 
         let p = local.as_ref();
         let meta = fs::metadata(p)?;
 
         let name = p.file_name().unwrap();
-        let mut filename : PathBuf= format!("{}", uuid).into();
+        let mut filename: PathBuf = dir.into();
         filename = filename.join(name);
 
-        let mut remote_file = self
-            .sess
-            .scp_send(&filename, 0o644, meta.len(), None)?;
+        let mut remote_file = self.sess.scp_send(&filename, 0o644, meta.len(), None)?;
 
         let mut f = File::open(p)?;
         io::copy(&mut f, &mut remote_file)?;
@@ -106,20 +118,54 @@ impl Connection {
         let code = self.exec(&cmd)?;
         if code != 0 {
             let err = format!("exec {} failed", cmd);
-            return Err(Error::Cmd(err))
+            return Err(Error::Cmd(err));
         }
 
         Ok(())
     }
 
+    fn create_dir(&self) -> Result<String, Error> {
+        //create a tmp directory
+        let uuid = Uuid::new_v4();
+        let mkdir = format!("mkdir {}", uuid);
+        let code = self.exec(&mkdir)?;
+        if code != 0 {
+            let err = format!("create dir {} failed", uuid);
+            return Err(Error::Cmd(err));
+        }
+        Ok(format!("{}", uuid))
+    }
+
     fn exec(&self, cmd: &str) -> Result<i32, Error> {
         let mut channel = self.sess.channel_session()?;
         channel.exec(&cmd)?;
-        
+
         let mut s = String::new();
         channel.read_to_string(&mut s)?;
         print!("{}", s);
         channel.wait_close()?;
         Ok(channel.exit_status()?)
+    }
+}
+
+fn extract_file_name(remote: &str) -> Option<String> {
+    let addr: Vec<_> = remote.split(":").collect();
+    if addr.len() != 2 {
+        return None;
+    }
+
+    let p: &Path = addr[1].as_ref();
+    let ret = p.file_name()?.to_string_lossy().into_owned();
+    Some(ret)
+}
+
+struct Clean<'a>(&'a Connection, String);
+impl<'a> Drop for Clean<'a> {
+    fn drop(&mut self) {
+        let cmd = format!("rm -fr {}", self.1);
+        let code = self.0.exec(&cmd).expect("clean failed");
+        if code != 0 {
+            println!("clean failed");
+        }
     }
 }
