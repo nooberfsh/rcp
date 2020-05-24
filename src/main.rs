bf05ff1ed40c1a48f42d5e3cff841af35dbca84e
nooberfsh <nooberfsh@gmail.com>
2020-05-24 22:41:32 +0800

    add more error context

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,19 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::path::{Path, PathBuf};
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use ssh2::Session;
+use std::process::exit;
 use uuid::Uuid;
 
 fn main() {
-    //TODO: this may be configured through command line argument
-    let home_dir = dirs::home_dir().expect("fetch home dir failed");
-    let config_path = home_dir.join(".rcp");
-    let config = read_config(&config_path).unwrap();
+    let config = read_config().unwrap();
     let scp = config.scp();
 
     let args: Vec<_> = env::args().collect();
     if args.len() != 3 {
         println!("Usage: rcp from to");
-        return;
+        exit(1)
     }
     let conn = Connection::connect(&config).unwrap();
     if is_remote_addr(&args[1]) {
@@ -29,15 +27,21 @@ fn main() {
     } else if is_remote_addr(&args[2]) {
         conn.send(&args[1], &args[2], scp).unwrap();
     } else {
-        println!("can not a remote addr")
+        println!("can not find a remote addr");
+        exit(1)
     }
+    println!("rcp success")
 }
 
-fn read_config<P: AsRef<Path>>(p: P) -> Result<config::Config> {
-    let mut f = File::open(p)?;
+fn read_config() -> Result<config::Config> {
+    let home_dir = dirs::home_dir().expect("home dir is not set");
+    let config_path = home_dir.join(".rcp");
+    let mut f = File::open(&config_path).with_context(|| "failed to open $HOME/.rcp")?;
     let mut s = String::new();
-    f.read_to_string(&mut s)?;
-    let config = toml::from_str(&s)?;
+    f.read_to_string(&mut s)
+        .with_context(|| "failed to read string from $HOME/.rcp")?;
+    let config =
+        toml::from_str(&s).with_context(|| "failed to deserialize $HOME/.rcp to config")?;
     Ok(config)
 }
 
@@ -52,12 +56,14 @@ impl Connection {
 
     // Connect to the local SSH server
     fn connect(config: &config::Config) -> Result<Self> {
-        let tcp = TcpStream::connect(config.addr()).expect("can not connect to the jumpserver");
-        let mut sess = Session::new().expect("initialize a session failed");
+        let tcp = TcpStream::connect(config.addr())
+            .with_context(|| "can not connect to the jump server")?;
+        let mut sess = Session::new().with_context(|| "initialize a session failed")?;
         sess.set_tcp_stream(tcp);
         sess.handshake()?;
 
-        sess.userauth_pubkey_file(&config.username, None, &config.private_key_path(), None)?;
+        sess.userauth_pubkey_file(&config.username, None, &config.private_key_path(), None)
+            .with_context(|| "failed to auth user")?;
         assert!(sess.authenticated());
         Ok(Connection::new(sess))
     }
@@ -67,7 +73,10 @@ impl Connection {
         let _clean = Clean(self, dir.clone());
 
         let cmd = format!("{} {} {}", scp, remote, dir);
-        self.exec(&cmd)?;
+        self.exec(&cmd)
+            .with_context(|| format!("failed to execute {} on jump server", scp))?;
+
+        println!("recv file from remote server to jump server success");
 
         let name = match extract_file_name(remote) {
             Some(f) => f,
@@ -90,7 +99,7 @@ impl Connection {
 
         let (mut remote_file, _) = self.sess.scp_recv(&remote)?;
         io::copy(&mut remote_file, &mut local_file)?;
-        println!("copy to local success");
+        println!("recv file from jump serve to local success");
         Ok(())
     }
 
@@ -107,16 +116,18 @@ impl Connection {
 
         let mut remote_file = self.sess.scp_send(&filename, 0o644, meta.len(), None)?;
 
-        let mut f = File::open(p)?;
-        io::copy(&mut f, &mut remote_file)?;
+        let mut f = File::open(p).with_context(|| "failed to open send file")?;
+        io::copy(&mut f, &mut remote_file)
+            .with_context(|| "failed to copy send file to jump server")?;
         drop(remote_file);
-        println!("copy file to jump server success");
+        println!("send file to jump server success");
 
         let cmd = format!("{} {} {}", scp, filename.to_str().unwrap(), remote);
         let code = self.exec(&cmd)?;
         if code != 0 {
             Err(anyhow!("exec {} failed", cmd))
         } else {
+            println!("send file to remote server success");
             Ok(())
         }
     }
@@ -139,7 +150,6 @@ impl Connection {
 
         let mut s = String::new();
         channel.read_to_string(&mut s)?;
-        print!("{}", s);
         channel.wait_close()?;
         Ok(channel.exit_status()?)
     }
